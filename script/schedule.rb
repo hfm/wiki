2ec05a7ff1699e30263b8dee67ce159f9b8924b1
Okumura Takahiro <hfm.garden@gmail.com>
2014-02-02 03:14:39 +0900

    path間違ってた

--- script/schedule.rb
+++ script/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "/var/log/whenever.log"
 
 every 1.minutes do
-  command "/usr/local/ruby-2.1.0/ruby /var/www/wiki/script/push_remote.rb"
+  command "/usr/local/ruby-2.1.0/bin/ruby /var/www/wiki/script/push_remote.rb"
 end
