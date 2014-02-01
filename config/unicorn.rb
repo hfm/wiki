24b831fb133dca8f7b6c9e1e06025ff0e0529772
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-02-02 00:48:04 +0900

    unicornのログのパスを変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,8 +7,8 @@ listen "unix:/var/run/unicorn_wiki.sock", :backlog => 1024
 
 pid "/var/run/unicorn.pid"
 
-stderr_path "/var/log/nginx/unicorn.stderr.log"
-stdout_path "/var/log/nginx/unicorn.stdout.log"
+stderr_path "/var/log/unicorn/stderr.log"
+stdout_path "/var/log/unicorn/stdout.log"
 
 preload_app true
 check_client_connection false
